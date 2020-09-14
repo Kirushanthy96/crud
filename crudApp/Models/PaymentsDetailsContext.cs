bcb042a02bf685ee7ef9b8c59b82e1cf173bb9d1
kirushanthy <39116761+kirushanthy@users.noreply.github.com>
2020-09-15 00:36:28 +0530

    first

---
+++ crudApp/Models/PaymentsDetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crudApp.Models
+{
+    public class PaymentsDetailsContext:DbContext
+    {
+        public PaymentsDetailsContext(DbContextOptions<PaymentsDetailsContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+    }
+}
